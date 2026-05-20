00f1d93056c08fc2bbc8492540b45c7c77be5b68
Laptop <mark.sarkozi20@gmail.com>
2026-05-20 04:16:41 +0300

    random tries 19

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000/api";
+const API = "/api";
 const newsletter = document.getElementById("newsletterSection");
 
 let currentUser = JSON.parse(localStorage.getItem("currentUser"));
